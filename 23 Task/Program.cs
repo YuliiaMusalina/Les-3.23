35b4d0679317a8d5c98ec80e9a27c139e08d5cdc
Ulia Kamalova <mysula@mail.ru>
2022-08-17 13:44:13 +0500

    23 задача

---
+++ 23 Task/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = ReadInt("Ведите число: ");
+int i = 1;
+
+while(i <= number)
+{
+    Console.Write(Sqr(i) + " ");
+    i++;
+}
+
+int Sqr(int a)
+{
+    return a * a * a;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
